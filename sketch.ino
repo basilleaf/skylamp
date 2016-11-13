bd06adf00c3a93dc32ccbf26bd165540030bde1c
Lisa Ballard <lballard.cat@gmail.com>
2016-11-13 12:01:11 -0800

    adds pin commands to arduino code

--- sketch.ino
+++ sketch.ino
@@ -1,8 +1,15 @@
 String msg = " ";
+int myPins[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
 void setup() {
   Serial.begin(9600); // set the baud rate
   Serial.println("Ready"); // print "Ready" once
+
+  int i;
+  for (i = 0; i < 15; i = i + 1) {
+    pinMode(myPins[i], OUTPUT);
+  }
+
 }
 
 void loop() {
@@ -11,19 +18,21 @@ void loop() {
 
   while(Serial.available()) { // only send data back if data has been sent
 
+    // turn them all off first
+    int i;
+    for (i = 0; i < 15; i = i + 1) {
+      digitalWrite(myPins[i], LOW);
+    }
+
     char inByte = Serial.read(); // read the incoming data
 
-    Serial.println(inByte);
-
     if (inByte == ';') {
-      Serial.println("if");
       Serial.println(msg);
+      digitalWrite(myPins[msg.toInt()], HIGH);
       msg = "";
 
     } else {
-      Serial.println("else");
       msg += inByte;
-      Serial.println(msg);
     }
   }
   delay(100); // delay for 1/10 of a second

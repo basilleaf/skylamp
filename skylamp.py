aa92f7b1d13a563153e0835210bd62434e9dcdaa
Lisa Ballard <lballard.cat@gmail.com>
2016-11-12 19:36:33 -0800

    improves var name

--- skylamp.py
+++ skylamp.py
@@ -1,19 +1,20 @@
 import ephem
 
-kpno = ephem.Observer()
+location = ephem.Observer()
 
-kpno.long = ephem.degrees("122.4194")
-kpno.lat = ephem.degrees("37.7749")
-kpno.date = "2016/11/12 19:50:300"
+location.long = ephem.degrees("-122.4194")
+location.lat = ephem.degrees("37.7749")
+location.date = "2016/11/12 17:00:300"
 
-jupiter = ephem.Jupiter(kpno)
-saturn = ephem.Saturn(kpno)
-mercury = ephem.Mercury(kpno)
-venus = ephem.Venus(kpno)
-mars = ephem.Mars(kpno)
+jupiter = ephem.Jupiter(location)
+saturn = ephem.Saturn(location)
+mercury = ephem.Mercury(location)
+venus = ephem.Venus(location)
+mars = ephem.Mars(location)
 
-print jupiter.alt, jupiter.az
-print saturn.alt, saturn.az
-print mercury.alt, mercury.az
-print venus.alt, venus.az
-print mars.alt, mars.az
+# Azimuth usually starts from North
+print jupiter.alt, jupiter.az, jupiter.mag
+print saturn.alt, saturn.az, saturn.mag
+print mercury.alt, mercury.az, mercury.mag
+print venus.alt, venus.az, venus.mag
+print mars.alt, mars.az, mars.mag

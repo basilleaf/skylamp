7cb53017eb200668d9dcd40b70fca673fce05e96
Lisa Ballard <lballard.cat@gmail.com>
2016-11-29 19:12:55 -0800

    fixes #3

--- skylamp.py
+++ skylamp.py
@@ -31,19 +31,22 @@ pin_codes = {
     ('saturn','W'): 15
 }
 
-current_date_time = "2016/11/30 01:50:00"
+current_date_time = "2016/11/30 02:55:00"
 
 # setup pyephem Observer location
 location = ephem.Observer()
-location.lat = ephem.degrees(37.7749)
-location.long = ephem.degrees(-122.4194)
+location.lat = ephem.degrees('37.7749')
+location.long = ephem.degrees('-122.4194')
+location.date = ephem.Date(current_date_time)
 
 mer = ephem.Mercury(location)
 ven = ephem.Venus(location)
 mar = ephem.Mars(location)
 jup = ephem.Jupiter(location)
 sat = ephem.Saturn(location)
-location.date = current_date_time
+
+mar.compute(location)
+print(mar.alt, mar.az, mar.mag)
 
 # Azimuth in pyephome starts from North and runs clockwise
 planets = {
@@ -59,28 +62,30 @@ for p, i in planets.items():
 
     alt, az, mag = i
 
-    if 180 * alt / pi < 5:
+    alt_deg = 180 * alt / pi
+    if alt_deg < 5:
         continue # planet is below the horizon
 
-    az_degrees = 180 * az / pi
-    if az_degrees <= 135:
+    az_deg = 180 * az / pi
+    if az_deg <= 135:
         # planet is in the East
         direction = "E"
 
-    if az_degrees > 135 and az_degrees < 225:
+    if az_deg > 135 and az_deg < 225:
         # planet is in the South
         direction = "S"
 
-    if az_degrees >= 135:
+    if az_deg >= 135:
         # planet is in the West
         direction = "W"
 
     # if direction:
-    msg = pin_codes[(p, direction)]
-    print(p, direction, msg)
-
-    msg = str(msg) + ';'
-    print(msg)
+    pin = pin_codes[(p, direction)]
+    msg = (p, direction, pin)
+    if debug:
+        print(msg)
+        print('alt: %s, az: %s, mag: %s' % (str(alt_deg), str(az_deg), str(mag)))
+        print()
     if not debug:
         ser.write(str(msg) + ';')
 

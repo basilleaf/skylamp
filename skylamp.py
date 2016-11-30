37a9b9613b685ca41e16beccae7eded63411c3e7
Lisa Ballard <lballard.cat@gmail.com>
2016-11-29 17:56:40 -0800

    bit of code cleanup, removes demo randomizer stuff, adds debug mode for testing without arduino connected

--- skylamp.py
+++ skylamp.py
@@ -1,22 +1,19 @@
+from __future__ import print_function
 from math import pi
 from time import sleep
 import ephem
 import serial
 
 # setup serial port where arduino is
-ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
+debug = False
+try:
+    ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
+except serial.SerialException:
+    print("\nNo USB connection, continuing in debug mode... \n")
+    debug = True
 
-# Azimuth in pyephome starts from North and runs clockwise
-planets = {
-    'mercury': (mer.alt, mer.az, mer.mag),
-    'venus': (ven.alt, ven.az, ven.mag),
-    'mars': (mar.alt, mar.az, mar.mag),
-    'jupiter': (jup.alt, jup.az, jup.mag),
-    'saturn': (sat.alt, sat.az, sat.mag),
-}
-
-# nasty hacker
-pin_code = {
+# match the arduino pin to a planet and direction in sky
+pin_codes = {
     ('mercury','E'): 1,
     ('mercury','C'): 2,
     ('mercury','W'): 3,
@@ -34,53 +31,58 @@ pin_code = {
     ('saturn','W'): 15
 }
 
-demo_dates = ["2016/12/11 03:40:300", "2016/01/01 04:00:000", "2016/01/21 04:00:000", "2016/19/8 11:00:000"]  # must be GMT
+current_date_time = "2016/11/30 01:50:00"
 
-while True:
-    for k,d in enumerate(demo_dates):
+# setup pyephem Observer location
+location = ephem.Observer()
+location.lat = ephem.degrees(37.7749)
+location.long = ephem.degrees(-122.4194)
 
-        # setup pyephem Observer location
-        location = ephem.Observer()
-        location.lat = ephem.degrees(37.7749)
-        location.long = ephem.degrees(-122.4194)
+mer = ephem.Mercury(location)
+ven = ephem.Venus(location)
+mar = ephem.Mars(location)
+jup = ephem.Jupiter(location)
+sat = ephem.Saturn(location)
+location.date = current_date_time
 
-        mer = ephem.Mercury(location)
-        ven = ephem.Venus(location)
-        mar = ephem.Mars(location)
-        jup = ephem.Jupiter(location)
-        sat = ephem.Saturn(location)
-        location.date = d[0]
+# Azimuth in pyephome starts from North and runs clockwise
+planets = {
+    'mercury': (mer.alt, mer.az, mer.mag),
+    'venus': (ven.alt, ven.az, ven.mag),
+    'mars': (mar.alt, mar.az, mar.mag),
+    'jupiter': (jup.alt, jup.az, jup.mag),
+    'saturn': (sat.alt, sat.az, sat.mag),
+}
 
-        print "checking date: %s" % d
+# < 135 East light -- 135 to 225 Center Light -- > 225 West light
+for p, i in planets.items():
 
-        # < 135 East light -- 135 to 225 Center Light -- > 225 West light
-        for p, i in planets.items():
+    alt, az, mag = i
 
-            alt, az, mag = i
+    if 180 * alt / pi < 5:
+        continue # planet is below the horizon
 
-            if 180 * alt / pi < 5:
-                continue # planet is below the horizon
+    az_degrees = 180 * az / pi
+    if az_degrees <= 135:
+        # planet is in the East
+        direction = "E"
 
-            az_degrees = 180 * az / pi
-            if az_degrees <= 135:
-                # planet is in the East
-                direction = "E"
+    if az_degrees > 135 and az_degrees < 225:
+        # planet is in the South
+        direction = "S"
 
-            if az_degrees > 135 and az_degrees < 225:
-                # planet is in the South
-                direction = "S"
+    if az_degrees >= 135:
+        # planet is in the West
+        direction = "W"
 
-            if az_degrees >= 135:
-                # planet is in the West
-                direction = "W"
+    # if direction:
+    msg = pin_codes[(p, direction)]
+    print(p, direction, msg)
 
-            # if direction:
-            msg = pin_code[(p, direction)]
-            print p, direction, msg
+    msg = str(msg) + ';'
+    print(msg)
+    if not debug:
+        ser.write(str(msg) + ';')
 
-            ser.write(str(msg) + ';')
-            del msg
-            del direction
-
-        del location
-        sleep(5)
+    del msg
+    del direction
